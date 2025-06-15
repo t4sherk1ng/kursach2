7f984ea699fd90611a37ab6f9f4017cd8c84ff87
Filipp <restokov@yandex.ru>
2025-06-16 00:41:44 +0300

    added repos

---
+++ src/main/java/org/example/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package org.example.repository;
+
+
+import org.example.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
