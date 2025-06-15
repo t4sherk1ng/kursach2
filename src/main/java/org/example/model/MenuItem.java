afeebd5915a8b28c444db46b50a5d944da2ffb99
Filipp <restokov@yandex.ru>
2025-06-16 00:36:46 +0300

    added restaurant and menu item models

---
+++ src/main/java/org/example/model/MenuItem.java
@@ -0,0 +1,25 @@
+package org.example.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "menu_item")
+@Getter
+@Setter
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private boolean available = true;
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
