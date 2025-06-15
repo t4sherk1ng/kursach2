4df8650a8aab67a88a956669f7632affde7fa3a3
Filipp <restokov@yandex.ru>
2025-06-16 00:39:12 +0300

    added order models

---
+++ src/main/java/org/example/model/Order.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_items",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private Set<MenuItem> items = new HashSet<>();
+
+    private double totalPrice;
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
---
+++ src/main/java/org/example/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.example.model;
+
+public enum OrderStatus {
+    CREATED, PROCESSING, DELIVERED, CANCELLED
+}
