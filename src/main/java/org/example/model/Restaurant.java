0146b43765e0ccef713b3dc661a99c30e6ca93bf
Filipp <restokov@yandex.ru>
2025-06-16 00:39:16 +0300

    added order models

---
+++ src/main/java/org/example/model/Restaurant.java
@@ -0,0 +1,25 @@
+package org.example.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuItem> menu = new ArrayList<>();
+}
